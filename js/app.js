d6d56e46b290f76f4d2c5d8786f4eb2c0f50691a
garden-profi <79133236+garden-profi@users.noreply.github.com>
2021-02-16 10:12:00 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+
+        //swiper
+        var mySwiper = new Swiper ('.swiper-container', {
+            effect: 'coverflow',
+            loop: true,
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            },
+            keyboard: {
+              enabled: true,
+            },
+            pagination: {
+              el: '.swiper-pagination',
+                dynamicBullets: true,
+            },
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+        })
+    
+    
+    $(window).on('load scroll', function(){
+        if($(window).scrollTop() > 0){
+            $('header').addClass('scrolled');
+        }else{
+            $('header').removeClass('scrolled');
+        }
+    });
+
+
+    $(".page").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
+    });     
+
+    });
